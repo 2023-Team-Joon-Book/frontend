3f59cf0f9fc478726d77a541eb53042a18c8f707
Heekyeong Park <qkrgmlrud00@gmail.com>
2023-04-13 00:06:13 +0900

    feat: 데이터 타입 변경 (인터페이스) 및 css 수정

--- src/pages/ReadingShelf.tsx
+++ src/pages/ReadingShelf.tsx
@@ -1,62 +1,40 @@
 import React,{useState, useEffect} from "react";
+import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import NavigationBar from "../components/NavigationBar";
 import Header from "../components/Header";
 import "../scss/MyShelf.scss";
 import axios from "axios";
 
+interface Data {
+  id: number;
+  author: string;
+  coverImageUrl: string;
+  height: number;
+  publisher: string;
+  thickness: number;
+  title: string;
+  width: number;
+}
+
 function ReadingShelf() {
-  const [bookList, setBookList] = useState([
-    {
-      author: "",
-      coverImageUrl: "",
-      height: 0,
-      publisher: "",
-      thickness: 0, 
-      title: "",
-      width: 0
-      }
-  ]);
-  // const reading = [
-  //   {
-  //     image: "image",
-  //     title: "Guy",
-  //     author: "Jowwita Bydlowska",
-  //   },
+  const [data, setData] = useState<Data[]>();
 
-  //   {
-  //     image: "image",
-  //     title: "Guy",
-  //     author: "Jowwita Bydlowska",
-  //   },
-
-  //   {
-  //     image: "image",
-  //     title: "Guy",
-  //     author: "Jowwita Bydlowska",
-  //   },
-
-  //   {
-  //     image: "image",
-  //     title: "Guy",
-  //     author: "Jowwita Bydlowska",
-  //   },
-  // ];
   localStorage.setItem('id', '4');
   const user = localStorage.getItem('id');
 
   // 읽는 책 조회 
   const allList = async () =>{
     const response = await axios.get(`http://localhost:8080/api/v1/readings/${user}?status=READING`)
-    setBookList(response.data)
+    setData(response.data)
     return response.data;
-    console.log(response.data)
   }
 
   // 컴포넌트가 실행될 때 바로 보이게 
   useEffect(()=>{
-    const getAllList=async () =>{
+    const getAllList = async () =>{
       const allBookList = await allList();
-      if(allBookList) setBookList(allBookList);
+      if(allBookList) setData(allBookList);
     };
     getAllList();
   // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -68,8 +46,10 @@ function ReadingShelf() {
         <Header />
       </div>
       <div className="container">
-        {bookList?.map((e) => (
-          <img className="book" src={e.coverImageUrl} alt="이미지" />
+        {data?.map((e) => (
+          <Link to={`/detail/${e.id}`}>
+            <img className="book" src={e.coverImageUrl} alt="이미지"/> 
+          </Link>
         ))}
       </div>
       <div className="navbar">
@@ -77,6 +57,22 @@ function ReadingShelf() {
       </div>
     </div>
   );
+  // const Book = ({ data }: { data: Data }) => {
+  //   return (
+  //     <div className="std">
+  //     <div> 
+  //        <Header />
+  //     </div>
+  //     <div className="container">
+  //         <Link to={`/detail/${data.id}`}>
+  //           <img className="book" src={data.coverImageUrl} alt="이미지"/> 
+  //         </Link>
+  //     </div>
+  //     <div className="navbar">
+  //       <NavigationBar />
+  //     </div>
+  //   </div>
+  //   );
+  // }
 }
-
 export default ReadingShelf;
