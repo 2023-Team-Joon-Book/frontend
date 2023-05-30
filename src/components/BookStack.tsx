b5a39e296a7a3b2e3d042dacf3594459ee91619b
KoneJ <kbvv67890@gmail.com>
2023-05-30 22:08:04 +0900

    feat: 책 이름 불러오기 통신 완료

--- src/components/BookStack.tsx
+++ src/components/BookStack.tsx
@@ -1,44 +1,46 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+interface Book {
+  id: number;
+  title: string;
+  left: number;
+  top: number;
+  color: string;
+  textColor: string;
+}
 
 const BookStack = () => {
-  const [books, setBooks] = useState<{ id: number; width: number; height: number; left: number; top: number; color: string; text: string; textColor: string; }[]>([]);
+  const [books, setBooks] = useState<Book[]>([]);
 
   const handleResize = () => {
     setBooks(prevBooks => {
-      // 화면 크기 변경 시, 책의 위치를 재조정하여 화면 내에 유지
-      return prevBooks.map(book => ({
+      return prevBooks.map((book, index) => ({
         ...book,
-        left: Math.min(Math.max(book.left, 0), window.innerWidth - book.width),
-        top: Math.min(Math.max(book.top, 0), window.innerHeight - book.height),
+        left: Math.min(Math.max(book.left, 0), window.innerWidth - 150),
+        top: Math.min(Math.max(book.top, 0), window.innerHeight - 23 - index * 2),
       }));
     });
   };
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      if (books.length >= 20) {
-        clearInterval(interval);
-        return;
-      }
+    axios.get('http://localhost:8080/api/v1/readings/1?status=READ')
+      .then(response => {
+        const booksData = response.data.map((bookData: any, index: number) => ({
+          id: bookData.id,
+          title: bookData.title,
+          left: Math.floor(window.innerWidth * 0.5 - 75 + Math.random() * 100),
+          top: Math.floor(window.innerHeight * 0.03 + (window.innerHeight * 0.033 * index) - 20),
+          color: index % 2 === 0 ? 'DCF1EB' : '75C8B0',
+          textColor: index % 2 === 0 ? '#000000' : '#FFFFFF',
+        }));
 
-      const newBook = {
-        id: books.length + 1,
-        width: Math.floor(150 + Math.random() * 81), // 랜덤한 책의 너비 (150에서 250 사이의 값)
-        height: Math.floor(23 + Math.random() * 7), // 랜덤한 책의 높이 (100에서 200 사이의 값)
-        left: Math.floor(window.innerWidth * 0.5 - 50 + Math.random() * 100), // 화면 가로 중앙에 배치
-        top: Math.floor(window.innerHeight * 0.03 + (window.innerHeight * 0.033 * books.length) - 20), // 화면 위에서 8% 위로 쌓이도록 배치
-        color: books.length % 2 === 0 ? 'DCF1EB' : '75C8B0', // 홀수/짝수 번째에 따라 색상 설정
-        text: `거침없이 하이킥${books.length + 1}`, // 책 안에 들어가는 텍스트 설정
-        textColor: books.length % 2 === 0 ? '#000000' : '#FFFFFF', // 홀수/짝수 번째에 따라 글씨 색상 설정
-      };
-
-      setBooks(prevBooks => [newBook, ...prevBooks]);
-    }, 300);
-
-    return () => {
-      clearInterval(interval);
-    };
-  }, [books]);
+        setBooks(booksData);
+      })
+      .catch(error => {
+        console.error('Error fetching books', error);
+      });
+  }, []);
 
   useEffect(() => {
     window.addEventListener('resize', handleResize);
@@ -48,7 +50,7 @@ const BookStack = () => {
   }, []);
 
   useEffect(() => {
-    handleResize(); // 초기 화면 크기 설정을 위해 한 번 호출
+    handleResize();
   }, []);
 
   return (
@@ -58,18 +60,18 @@ const BookStack = () => {
           key={book.id}
           className="absolute flex items-center justify-center shadow-md rounded-md"
           style={{
-            width: book.width,
-            height: book.height,
-            left: book.left - book.width / 2, // 중앙 정렬을 위해 width의 절반만큼 왼쪽으로 이동
-            top: book.top + index * 2, // 책들이 겹치도록 간격 조정
+            width: 150,
+            height: 23,
+            left: book.left - 150 / 2,
+            top: book.top + index * 2,
             transition: 'all 0.5s',
-            zIndex: 20 - index, // 쌓인 책의 zIndex를 조정하여 겹치게 함
-            overflow: 'hidden', // 책이 짤리지 않도록 overflow 설정
-            backgroundColor: `#${book.color}`, // 배경색 설정
-            color: book.textColor, // 글씨 색상 설정
+            zIndex: 20 - index,
+            overflow: 'hidden',
+            backgroundColor: `#${book.color}`,
+            color: book.textColor,
           }}
         >
-          <span className="text-sm text-center">{book.text}</span>
+          <span className="text-sm text-center">{book.title}</span>
         </div>
       ))}
     </div>
@@ -77,3 +79,4 @@ const BookStack = () => {
 };
 
 export default BookStack;
+
