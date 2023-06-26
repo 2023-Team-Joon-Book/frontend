e215ed9a14e3af8fc656c02df1559d3264e96f5e
KoneJ <kbvv67890@gmail.com>
2023-06-26 18:58:38 +0900

    fix: 퍼센트 렌더링 방식 수정 1차

--- src/components/DetailPercent.tsx
+++ src/components/DetailPercent.tsx
@@ -5,15 +5,14 @@ import axios from 'axios';
 
 interface DetailPercentProps {
   totalPages: number;
-  bookId: string; 
+  bookId: string;
 }
 
-function DetailPercent({ totalPages,  bookId }: DetailPercentProps) {
-  
+function DetailPercent({ totalPages, bookId }: DetailPercentProps) {
   const [currentPage, setCurrentPage] = useState<number>(0);
+  const [currentPersent, setCurrentPersent] = useState<number>(0);
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const [inputValue, setInputValue] = useState<number | null>(null);
-  const percent: number = (currentPage / totalPages) * 100;
 
   useEffect(() => {
     const fetchBook = async () => {
@@ -22,14 +21,15 @@ function DetailPercent({ totalPages,  bookId }: DetailPercentProps) {
           throw new Error('bookId is undefined');
         }
         const response = await axios.get(`http://localhost:8080/api/v1/readings/percentages/1?bid=${bookId}`);
-        setCurrentPage(response.data.data);
+        setCurrentPersent(response.data.data);
+        
       } catch (error) {
-        console.error("Error fetching book data:", error);
+        console.error('Error fetching book data:', error);
       }
     };
-  
+
     fetchBook();
-  }, [bookId]);
+  }, [bookId] );
 
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -39,13 +39,17 @@ function DetailPercent({ totalPages,  bookId }: DetailPercentProps) {
 
   const handleModalSubmit = () => {
     if (inputValue !== null && inputValue >= 1 && inputValue <= totalPages) {
-      setCurrentPage(inputValue);
       if (bookId === undefined) {
         throw new Error('bookId is undefined');
       }
-      axios.put(`http://localhost:8080/api/v1/readings/1`, { bookId: parseInt(bookId), lastPage: inputValue })
-        .catch(error => {
-          console.error("Error updating pages:", error);
+      axios
+        .put(`http://localhost:8080/api/v1/readings/1`, { bookId: parseInt(bookId), lastPage: inputValue })
+        .then(response => {
+          // Assuming the response contains the current page and percentage
+          setCurrentPage(response.data.data.lastPage);
+        })
+        .catch((error) => {
+          console.error('Error updating pages:', error);
         });
     }
     setIsModalOpen(false);
@@ -58,20 +62,17 @@ function DetailPercent({ totalPages,  bookId }: DetailPercentProps) {
   return (
     <div className="mx-auto">
       <div className="relative">
-        <div
-          className="absolute bottom-0 left-0 right-0 bg-gray-300 rounded-full"
-          style={{ height: '6px' }}
-        ></div>
+        <div className="absolute bottom-0 left-0 right-0 bg-gray-300 rounded-full" style={{ height: '6px' }}></div>
         <div
           className="absolute bottom-0 left-0 bg-white rounded-full"
-          style={{ width: `${percent}%`, height: '6px' }}
+          style={{ width: `${currentPersent}%`, height: '6px' }}
         ></div>
       </div>
-        <div className="mt-2 flex justify-between">
-            <div className="text-gray-500 text-lg">{Math.round(percent)}%</div>
-            <div className="text-gray-500 text-lg">{`${currentPage}/${totalPages}`}</div>
-        </div>
-      <button 
+      <div className="mt-2 flex justify-between">
+        <div className="text-gray-500 text-lg">{Math.round(currentPersent)}%</div>
+        <div className="text-gray-500 text-lg">{`${currentPage}/${totalPages}`}</div>
+      </div>
+      <button
         onClick={handleOpenModal}
         style={{ backgroundColor: '#BFC66A' }}
         className="mt-4 w-full  text-white font-bold py-2 px-4 rounded"
@@ -79,8 +80,7 @@ function DetailPercent({ totalPages,  bookId }: DetailPercentProps) {
         페이지 입력하기
       </button>
       <PagesModal isOpen={isModalOpen} onSubmit={handleModalSubmit} onCancel={() => setIsModalOpen(false)}>
-        <h3 
-        className="text-lg leading-6 font-medium text-gray-900" id="modal-title">
+        <h3 className="text-lg leading-6 font-medium text-gray-900" id="modal-title">
           페이지 수 입력
         </h3>
         <div className="mt-2">
