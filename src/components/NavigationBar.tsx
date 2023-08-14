64ab7c5b904757141542549bcdc7f301e62d0f6e
KoneJ <kbvv67890@gmail.com>
2023-08-14 19:26:53 +0900

    design: 하단 네비게이션 바 영역 고정

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,21 +1,26 @@
+import React from "react";
 import BookRoundedIcon from "@mui/icons-material/BookRounded";
 import InsightsIcon from "@mui/icons-material/Insights";
 import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
 import { useNavigate } from "react-router-dom";
 
+const navigationBarStyles = {
+  position: "fixed",
+  bottom: 0,
+  left: 0,
+  width: "100%",
+  display: "flex",
+  justifyContent: "space-between",
+  backgroundColor: "white",
+  padding: "10px 40px",
+  boxShadow: "0px -2px 4px rgba(0, 0, 0, 0.1)",
+} as const; // Use 'as const' to specify the exact type of navigationBarStyles
+
 export default function NavigationBar() {
   const navigate = useNavigate();
 
   return (
-    <div
-      style={{
-        display: "flex",
-        textAlign: "center",
-        justifyContent: "space-between",
-        marginLeft: "40px",
-        marginRight: "40px",
-      }}
-    >
+    <div style={navigationBarStyles}>
       <div>
         <BookRoundedIcon
           onClick={() => {
