d508b8427d04aab2e32ae7a4b9df722235d934b1
kimsunjae <woal9844@gmail.com>
2023-09-16 01:38:29 +0900

    design : 페이지 전환시 넘어가는 효과 추가

--- src/components/shelf/Books.tsx
+++ src/components/shelf/Books.tsx
@@ -1,43 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
 interface BookProps {
-    title: string;
-    author: string;
-    description: string;
+  title: string
+  author: string
+  description: string
+  status: string
+}
+
+const Book: React.FC<BookProps> = ({ title, author, description, status }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const openModal = () => {
+    setIsModalOpen(true)
   }
-  
-  const Book: React.FC<BookProps> = ({ title, author, description }) => {
-    const [isModalOpen, setIsModalOpen] = useState(false);
-  
-    const openModal = () => {
-      setIsModalOpen(true);
-    };
-  
-    const closeModal = () => {
-      setIsModalOpen(false);
-    };
-  
-    return (
-      <div>
-        <div onClick={openModal} className="cursor-pointer">
-          <h2>{title}</h2>
-          <p>{author}</p>
-        </div>
-  
-        {isModalOpen && (
-          <div className="">
-            <div className="modal">
-              <button onClick={closeModal} className=" top-4 right-4 m-2">
-                <span className="text-lg font-bold"></span>
-              </button>
-              <h2>{title}</h2>
-              <p>{author}</p>
-              <p>{description}</p>
-            </div>
-          </div>
-        )}
+
+  const closeModal = () => {
+    setIsModalOpen(false)
+  }
+
+  return (
+    <div>
+      <div onClick={openModal} className="cursor-pointer">
+        <h2>{title}</h2>
+        <p>{author}</p>
+        <p>Status: {status}</p>
       </div>
-    );
-  };
-  
-  export default Book;
+      {isModalOpen && (
+        <div className="">
+          <div className="modal">
+            <button onClick={closeModal} className=" top-4 right-4 m-2">
+              <span className="text-lg font-bold"></span>
+            </button>
+            <h2>{title}</h2>
+            <p>{author}</p>
+            <p>{description}</p>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default Book
