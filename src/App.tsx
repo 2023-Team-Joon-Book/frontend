c4ed0b3d0e024ee3dffee162e907187d2eed02d8
woohee <aldkgha@naver.com>
2023-09-15 19:20:41 +0900

    design: 검색페이지 퍼블리싱

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,34 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import WishShelf from "./pages/WishShelf";
-import ReadingShelf from "./pages/ReadingShelf";
-import EndShelf from "./pages/EndShelf";
-import Statistics from "./pages/Statistics";
-import BookSearch from "./pages/BookSearch";
-import MainPage from "./pages/MainPage"
-import LoginPage from "./pages/LoginPage";
-import SignUpPage from "./pages/SignUpPage";
-import SearchPage from "./pages/SearchPage";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import WishShelf from './pages/WishShelf'
+import ReadingShelf from './pages/ReadingShelf'
+import EndShelf from './pages/EndShelf'
+import Statistics from './pages/Statistics'
+import BookSearch from './pages/BookSearch'
+import MainPage from './pages/MainPage'
+import LoginPage from './pages/LoginPage'
+import SignUpPage from './pages/SignUpPage'
+import SearchPage from './pages/SearchPage'
 
 function App() {
   return (
-    <>
-    <SearchPage />
-    {/* 
-      <Router>
-        <Routes>
-          원준
-          <Route path="/" element={<MainPage />} />
-          추후 담당자
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignUpPage />} />
-          우희
-          <Route path="/booksearch" element={<BookSearch />} />
-          선재
-          <Route path="/wish" element={<WishShelf />} />
-          <Route path="/reading" element={<ReadingShelf />} />
-          <Route path="/finish" element={<EndShelf />} />
-          추후 담당자
-          <Route path="/activity" element={<Statistics />} />
-        </Routes>
-      </Router>
-    */}
-    </>
-  );
+    <Router>
+      <Routes>
+        원준
+        <Route path="/" element={<MainPage />} />
+        추후 담당자
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        우희
+        <Route path="/booksearch" element={<SearchPage />} />
+        선재
+        <Route path="/wish" element={<WishShelf />} />
+        <Route path="/reading" element={<ReadingShelf />} />
+        <Route path="/finish" element={<EndShelf />} />
+        추후 담당자
+        <Route path="/activity" element={<Statistics />} />
+      </Routes>
+    </Router>
+  )
 }
 
-export default App;
+export default App
