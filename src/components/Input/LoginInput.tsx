b61506841d59612234202f290d0be003440c6532
woohee <aldkgha@naver.com>
2023-09-19 22:57:53 +0900

    feat: loginInput 컴포넌트 구현

--- src/components/Input/LoginInput.tsx
+++ src/components/Input/LoginInput.tsx
@@ -1,3 +1,4 @@
+import { ChangeEvent, forwardRef, KeyboardEventHandler } from 'react'
 import styled from 'styled-components'
 
 type LoginInputProps = {
@@ -5,26 +6,34 @@ type LoginInputProps = {
   value: string
   placeholder: string
   type: string
-  // onChange?: (e: ChangeEvent<HTMLInputElement>) => void
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void
+  onKeyPress?: KeyboardEventHandler<HTMLInputElement>
 }
 
-export default function LoginInput({ title, value, type, placeholder }: LoginInputProps) {
-  return (
-    <Layout>
-      <Label>{title}</Label>
-      <Input
-        value={value}
-        type={type}
-        // onChange={onChange}
-        placeholder={placeholder}></Input>
-    </Layout>
-  )
-}
+const LoginInput = forwardRef<HTMLInputElement, LoginInputProps>(
+  ({ title, value, type, placeholder, onChange, onKeyPress }, ref) => {
+    return (
+      <Layout>
+        <Label>{title}</Label>
+        <Input
+          value={value}
+          type={type}
+          onChange={onChange}
+          placeholder={placeholder}
+          ref={ref}
+          onKeyPress={onKeyPress}
+        />
+      </Layout>
+    )
+  },
+)
+
+LoginInput.displayName = 'LoginInput'
 
 const Layout = styled.div`
   display: flex;
   flex-direction: column;
-  margin-bottom: 2.8rem; // 줄임
+  margin-bottom: 2.8rem;
 `
 
 const Label = styled.label`
@@ -42,11 +51,19 @@ const Input = styled.input`
   height: 3.2rem;
   flex-shrink: 0;
   fill: rgba(255, 255, 255, 0.8);
-  border: 1px solid #000;
+  border: 1px solid #cac9c9;
   border-radius: 3rem;
   color: black;
   font-size: 1.2rem;
   font-style: normal;
   font-weight: 400;
   padding-left: 1.3rem;
+  &::placeholder {
+    color: silver;
+  }
+  &:focus::placeholder {
+    color: transparent;
+  }
 `
+
+export default LoginInput
