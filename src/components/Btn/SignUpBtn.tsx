2cce8b3214613a65a8c61f529b81021f34590b3f
woohee <aldkgha@naver.com>
2023-09-26 17:05:03 +0900

    refactor: signbtn props 추가

--- src/components/Btn/SignUpBtn.tsx
+++ src/components/Btn/SignUpBtn.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
 type SignUpBtnProps = {
+  onClick: () => void
   disabled?: boolean
 }
 
-const SignUpBtn: React.FC<SignUpBtnProps> = ({ disabled }) => (
+const SignUpBtn: React.FC<SignUpBtnProps> = ({ onClick, disabled }) => (
   <div className="fixed left-1/2 bottom-8 transform -translate-x-1/2 z-50">
     <button
+      onClick={onClick}
       className="w-80 h-16 rounded-full bg-opacity-80 bg-black flex items-center justify-center shadow-md"
       disabled={disabled}>
       <div className="text-white text-3xl" style={{ fontFamily: 'bmfont' }}>
