e2c51330b3ccf54310966254532311a0e12900e8
woohee <aldkgha@naver.com>
2023-10-28 19:59:43 +0900

    feat: 통계페이지 3개 헤더 글자색 변경

--- src/components/Header/MyHeader.tsx
+++ src/components/Header/MyHeader.tsx
@@ -86,7 +86,14 @@ const MyHeader: FC = () => {
         </button>
         <button
           className="text-black text-2xl px-4 py-2 mb-8 rounded-md"
-          style={{ color: location.pathname === '/choose' ? '#BFC66A' : 'black' }}
+          style={{
+            color:
+              location.pathname === '/choose' ||
+              location.pathname === '/statistics' ||
+              location.pathname === '/stack'
+                ? '#BFC66A'
+                : 'black',
+          }}
           onClick={goToStatistic}>
           독서 통계
         </button>
