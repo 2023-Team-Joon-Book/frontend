400d4ac27f09f7c074cddf1e52cf05d28b8fb5ed
woohee <aldkgha@naver.com>
2023-10-28 20:45:01 +0900

    design: 마우스 호버될때 크기, 그림자 설정

--- src/components/main/StartNavigator.tsx
+++ src/components/main/StartNavigator.tsx
@@ -2,15 +2,13 @@ import React from 'react'
 
 interface StartNavigatorProps {
   onClick: () => void
-  disabled: boolean
 }
 
-const StartNavigator: React.FC<StartNavigatorProps> = ({ onClick, disabled }) => (
+const StartNavigator: React.FC<StartNavigatorProps> = ({ onClick }) => (
   <div className="fixed left-1/2 bottom-8 transform -translate-x-1/2 z-50">
     <button
       onClick={onClick}
-      className="w-80 h-16 rounded-full bg-opacity-80 bg-black flex items-center justify-center shadow-md"
-      disabled={disabled}>
+      className="w-80 h-16 rounded-full bg-opacity-80 bg-black flex items-center justify-center shadow-md drop-shadow-lg transform transition-transform transition-shadow duration-300 hover:scale-105 hover:shadow-2xl">
       <div className="text-white text-3xl" style={{ fontFamily: 'bmfont' }}>
         시작하기
       </div>
