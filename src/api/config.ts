45602e611a521a3faf146f591e070881f3143e9e
KoneJ <kbvv67890@gmail.com>
2023-11-09 21:17:49 +0900

    chore: config 파일 수정

--- src/api/config.ts
+++ src/api/config.ts
@@ -5,19 +5,7 @@ const BASE_URL = 'http://localhost:8080/api/v1';
 export const baseInstance = axios.create({
   withCredentials: true,
   baseURL: BASE_URL, // 기본 URL 설정
-<<<<<<< HEAD
-})
-const token = localStorage.getItem('accessToken')
-if (token) {
-    baseInstance.defaults.headers.common['Authorization'] = `Bearer ${token}`
-}
-
-export const updateToken = (token: string) => {
-  baseInstance.defaults.headers.common['Authorization'] = `Bearer ${token}`
-}
-=======
 });
->>>>>>> develop
 
 const token = localStorage.getItem('accessToken');
 if (token) {
