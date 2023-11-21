a6c7b3bd1b6ee0b2da21e2b088682d9babe26949
KoneJ <kbvv67890@gmail.com>
2023-11-21 23:06:45 +0900

    feat: 메인페이지 autoSlider api 연동

--- src/components/main/ReverseSlider.tsx
+++ src/components/main/ReverseSlider.tsx
@@ -1,57 +1,55 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { baseInstance } from '../../api/config'; // Assuming this is your Axios instance
 import "../../scss/Slider.scss";
-import book1 from '../../../public/img/book1.png';
-import book2 from '../../../public/img/book2.png';
-import book3 from '../../../public/img/book3.png';
-import book4 from '../../../public/img/book4.png';
-import book5 from '../../../public/img/book5.png';
-import book6 from '../../../public/img/book6.png';
-import book7 from '../../../public/img/book7.png';
-import book8 from '../../../public/img/book8.png';
-import book9 from '../../../public/img/book9.png';
-import book10 from '../../../public/img/book10.png';
-import book11 from '../../../public/img/book11.png';
-import book12 from '../../../public/img/book12.png';
 
-const slides2 = [
-    { imgSrc: book1, target: "#", },
-    { imgSrc: book2, target: "#", },
-    { imgSrc: book3, target: "#", },
-    { imgSrc: book4, target: "#", },
-    { imgSrc: book5, target: "#", },
-    { imgSrc: book6, target: "#", },
-    { imgSrc: book7, target: "#", },
-    { imgSrc: book8, target: "#", },
-    { imgSrc: book9, target: "#", },
-    { imgSrc: book10, target: "#", },
-    { imgSrc: book11, target: "#", },
-    { imgSrc: book12, target: "#", },
-];
+interface Book {
+    imgSrc: string;
+    target: string;
+}
 
-export default function ReverseSlider() {
+const MainSlider = () => {
     const [animate, setAnimate] = useState(true);
+    const [books, setBooks] = useState<Book[]>([]);
+
+    useEffect(() => {
+        const fetchBooksData = async () => {
+            try {
+                const response = await baseInstance.get('/books/like');
+                if (response.data && response.data.data && response.data.data.content) {
+                    setBooks(response.data.data.content.map((book: any) => ({
+                        imgSrc: book.cover_image_url,
+                        target: "#"  // or any other target you need
+                    })));
+                }
+            } catch (error) {
+                console.error('Error fetching books data:', error);
+            }
+        };
+
+        fetchBooksData();
+    }, []);
+
     const onStop = () => setAnimate(false);
     const onRun = () => setAnimate(true);
 
     return (
         <div className="wrapper">
-            {/* 첫 번째 슬라이드 그룹 */}
             <div className="slide_container" onMouseEnter={onStop} onMouseLeave={onRun}>
                 <ul className="slide_wrapper">
-                    <div className={"slide original reverse".concat(animate ? "" : " stop")}>
-                        {slides2.map((s, i) => (
+                    <div className={"slide original".concat(animate ? "" : " stop")}>
+                        {books.map((s, i) => (
                             <li key={i} className="big">
                                 <div className="item" style={{ backgroundImage: `url(${s.imgSrc})` }}>
-                                    {/* {s.text} */}
+                                    {/* Additional content can be added here if needed */}
                                 </div>
                             </li>
                         ))}
                     </div>
-                    <div className={"slide clone reverse".concat(animate ? "" : " stop")}>
-                        {slides2.map((s, i) => (
+                    <div className={"slide clone".concat(animate ? "" : " stop")}>
+                        {books.map((s, i) => (
                             <li key={i} className="big">
                                 <div className="item" style={{ backgroundImage: `url(${s.imgSrc})` }}>
-                                    {/* {s.text} */}
+                                    {/* Additional content can be added here if needed */}
                                 </div>
                             </li>
                         ))}
@@ -60,4 +58,6 @@ export default function ReverseSlider() {
             </div>
         </div>
     );
-}
+};
+
+export default MainSlider;
