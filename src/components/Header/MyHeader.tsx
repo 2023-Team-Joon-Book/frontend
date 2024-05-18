be50757c4379ab93781eb0b9624ba438791eacce
woohee <aldkgha@naver.com>
2024-05-18 14:53:27 +0900

    design: 헤더 퍼블리싱 수정

--- src/components/Header/MyHeader.tsx
+++ src/components/Header/MyHeader.tsx
@@ -2,11 +2,16 @@ import { FC, useState } from 'react'
 import logo from '../../../public/logo.png'
 import { useLocation, useNavigate } from 'react-router-dom'
 import { baseInstance } from '../../api/config'
+import SearchBar from '../search/SearchBar'
+import axios from 'axios'
+import Swal from 'sweetalert2'
 
 const MyHeader: FC = () => {
   const navigate = useNavigate()
   const location = useLocation()
   const [isLogoutClicked, setIsLogoutClicked] = useState(false)
+  const [searchQuery, setSearchQuery] = useState<string>('')
+  const [books, setBooks] = useState<any[]>([])
 
   const handleLogout = async () => {
     setIsLogoutClicked(true)
@@ -51,44 +56,91 @@ const MyHeader: FC = () => {
     navigate('/')
   }
 
+  const handleSearch = async () => {
+    await fetchBooks(searchQuery)
+  }
+
+  // 책 정보를 가져오는 함수
+  const fetchBooks = async (query: string) => {
+    try {
+      const response = await axios.get('http://localhost:8081/api/v1/books/search', {
+        params: { title: query },
+      })
+      const fetchedBooks = response.data
+      const Toast = Swal.mixin({
+        toast: true,
+        position: 'top',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.onmouseenter = Swal.stopTimer
+          toast.onmouseleave = Swal.resumeTimer
+        },
+      })
+      Toast.fire({
+        icon: 'success',
+        title: '검색 성공!',
+      })
+      if (fetchedBooks.length === 0) {
+        // 데이터 없는지 체크
+        const Toast = Swal.mixin({
+          toast: true,
+          position: 'top',
+          showConfirmButton: false,
+          timer: 3000,
+          timerProgressBar: true,
+          didOpen: (toast) => {
+            toast.onmouseenter = Swal.stopTimer
+            toast.onmouseleave = Swal.resumeTimer
+          },
+        })
+        Toast.fire({
+          icon: 'warning',
+          title: '검색 결과가 없습니다.',
+        })
+      }
+      setBooks(fetchedBooks)
+    } catch (error) {
+      console.error('Failed to fetch books', error)
+      Swal.fire({
+        title: '네트워크 오류입니다!',
+        icon: 'error',
+      })
+    }
+  }
+
   const headerStyle = {
     width: '100%',
     backgroundColor: 'rgba(255, 255, 255, 0.92)',
-  }
-
-  const imageStyle = {
-    width: 'auto',
-    height: '144px',
+    display: 'flex',
+    justifyContent: 'space-between',
   }
 
   return (
     <div
-      className="fixed top-0 left-1/2 transform -translate-x-1/2 z-50 flex items-center justify-between w-full px-4"
+      className="fixed top-0 left-0 right-0 z-50 flex items-center justify-between w-full px-4"
       style={headerStyle}>
-      <div className="opacity-0 invisible">
-        <button className="text-black font-bold text-2xl px-4 py-2 mb-8 rounded-md">내 서재</button>
-        <button className="text-black font-bold px-4 text-2xl py-2 mb-8 rounded-md">
-          독서통계
-        </button>
-        <button className="text-black font-bold px-4 text-2xl py-2 mb-8 rounded-md">
-          로그아웃
-        </button>
+      <div className="flex justify-start items-center">
+        <img
+          src={logo}
+          alt="Header Image"
+          className="cursor-pointer m-6"
+          style={{ width: 'auto', height: '100px' }}
+          onClick={goToMain}
+        />
+        <SearchBar onSearch={handleSearch} onInputChange={setSearchQuery} />
       </div>
 
-      <img
-        src={logo}
-        alt="Header Image"
-        className="w-full mx-auto cursor-pointer"
-        style={imageStyle}
-        onClick={goToMain}
-      />
-
-      <div className="flex items-center space-x-4" style={{ fontFamily: 'Noto Sans KR' }}>
-        <button className="text-black  text-2xl px-4 py-2 mb-8 rounded-md" onClick={goToWish}>
+      <div
+        className="flex items-center justify-end space-x-4"
+        style={{ fontFamily: 'Noto Sans KR' }}>
+        <button className="text-black  text-sm py-2 mb-8 rounded-md" onClick={goToWish}>
           내 서재
         </button>
+        <p className="text-black text-sm px-2 py-2 mb-8 rounded-md">|</p>
         <button
-          className="text-black text-2xl px-4 py-2 mb-8 rounded-md"
+          className="text-black text-sm py-2 mb-8 rounded-md"
           style={{
             color:
               location.pathname === '/choose' ||
@@ -100,8 +152,9 @@ const MyHeader: FC = () => {
           onClick={goToStatistic}>
           독서 통계
         </button>
+        <p className="text-black text-sm px-2 py-2 mb-8 rounded-md">|</p>
         <button
-          className="text-black text-2xl px-4 py-2 mb-8 rounded-md"
+          className="text-black text-sm pr-2 py-2 mb-8 rounded-md"
           style={{
             color: isLogoutClicked ? '#BFC66A' : 'black', // 클릭 상태에 따라 색상 변경
           }}
